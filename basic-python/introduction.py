145181a009c45f083d251a36b430b4931da9c6d4
petsereypanha <sereypanhapet@gmail.com>
2025-11-04 14:14:21 +0700

    commit first

---
+++ basic-python/introduction.py
@@ -0,0 +1,48 @@
+# Store the pasta type as a string
+pasta_type = "spaghetti"
+
+# Store the quantity as an integer
+quantity = 80
+
+# Print the pasta type
+print(pasta_type)
+
+# Print the quantity
+print(quantity)
+
+# Update the pasta type to fusilli
+pasta_type = "fusilli"
+
+# Update the quantity to 100
+quantity = 100
+
+print(pasta_type)
+print(quantity)
+
+# Store the number of garlic cloves as an integer
+garlic_cloves = 3
+
+# Store the olive oil amount as a float
+olive_oil_tbsp = 2.5
+print(olive_oil_tbsp)
+
+# Increase the olive oil amount
+new_olive_oil_tbsp = 2.5 + 1.0
+
+print(garlic_cloves)
+print(new_olive_oil_tbsp)
+
+# Track if you have pasta at home
+has_pasta = True
+
+# Track if you have garlic
+has_garlic = False
+
+print(has_pasta)
+print(has_garlic)
+
+# Check the data type of olive_oil_tbsp
+print(type(olive_oil_tbsp))
+
+# Check the data type of has_pasta
+print(type(has_pasta))
