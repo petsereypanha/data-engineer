e7c8e7eb1d2065239a5ecf0f81e3265fea2f3beb
petsereypanha <sereypanhapet@gmail.com>
2025-11-05 23:47:59 +0700

    error handling

--- intermediate-python/lambda-functions-and-error-handling.py
+++ intermediate-python/lambda-functions-and-error-handling.py
@@ -26,3 +26,26 @@ sales = [125.97, 84.32, 99.78 ,154.21, 78.50, 83.67, 111.13]
 
 # Print the sales list
 print(sales)
+
+
+def clean_text(text):
+    # Attempt to clean the text
+    try:
+        return text.replace(" ", "_").lower()
+    # Run this code if an error occurs
+    except :
+        print("The clean_text() function expects a string as an argument, please check the data type provided!")
+clean_text(187)
+
+
+def clean_text(text):
+    # Check the data type
+    if type(text) == str:
+
+        return text.replace(" ", "_").lower()
+    else:
+        # Return a TypeError error if the wrong data type was used
+        raise TypeError("The clean_text() function expects a string as an argument, please check the data type provided!")
+
+
+clean_text("User Name 187")
