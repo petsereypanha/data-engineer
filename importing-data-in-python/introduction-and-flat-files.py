302320accef58b94096166bd030031754756a65d
petsereypanha <sereypanhapet@gmail.com>
2025-11-07 07:49:56 +0700

    importing flat files using numpy

--- importing-data-in-python/introduction-and-flat-files.py
+++ importing-data-in-python/introduction-and-flat-files.py
@@ -1,10 +1,70 @@
 # Open a file as read-only and bind it to file
-with open('moby_dick.txt', 'r') as file:
+with open('data/moby_dick.txt', 'r') as file:
     # Print it
     print(file.read())
 
 # Read & print the first 3 lines
-with open('moby_dick.txt') as file:
+with open('data/moby_dick.txt') as file:
     print(file.readline())
     print(file.readline())
     print(file.readline())
+
+# python
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Assign filename to variable: file
+file = 'data/digits.csv'
+
+data = pd.read_csv(file)
+
+print(data)
+
+# Load file as array: digits
+digits = np.loadtxt(file, delimiter=None)
+
+# Print datatype of digits
+print(digits.dtype)
+
+# Select and reshape a row
+im = digits[21, 1:]
+im_sq = np.reshape(im, (28, 28))
+
+# Plot reshaped data (matplotlib.pyplot already loaded as plt)
+plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.show()
+
+# Import numpy
+import numpy as np
+
+# Assign the filename: file
+file = 'data/digits_header.txt'
+
+# Load the data: data
+data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=[0, 2])
+
+# Print data
+print(data)
+
+# Assign filename: file
+file = 'data/seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import file as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
