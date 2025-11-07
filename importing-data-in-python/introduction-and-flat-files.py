4deb6718b76aa0dfa28db0331605024fda6037d7
petsereypanha <sereypanhapet@gmail.com>
2025-11-07 08:56:47 +0700

    importing flat files using pandas

--- importing-data-in-python/introduction-and-flat-files.py
+++ importing-data-in-python/introduction-and-flat-files.py
@@ -68,3 +68,44 @@ plt.scatter(data_float[:, 0], data_float[:, 1])
 plt.xlabel('time (min.)')
 plt.ylabel('percentage of larvae')
 plt.show()
+
+# Import pandas as pd
+import pandas as pd
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+# View the head of the DataFrame
+print(df.head())
+
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows=5, header=None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'data/titanic_corrupt.txt'
+
+data = pd.read_csv(file, sep='\t',comment='#', na_values='Nothing')
+# Print the head of the DataFrame
+
+print(data.to_string(index=False))
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
