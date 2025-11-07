46fb386316ef06c5065788fce74bb21575087e89
petsereypanha <sereypanhapet@gmail.com>
2025-11-07 10:29:19 +0700

    importing hdf5 file

--- importing-data-in-python/Importing-data.py
+++ importing-data-in-python/Importing-data.py
@@ -65,3 +65,43 @@ pd.DataFrame.hist(df[['disa10']])
 plt.xlabel('Extent of disease')
 plt.ylabel('Number of countries')
 plt.show()
+
+# Import packages
+import numpy as np
+import h5py
+
+# Assign filename: file
+file = 'data/test.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = group['Strain'][:]
+
+# Set number of time points to sample: num_samples
+num_samples = 1000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
