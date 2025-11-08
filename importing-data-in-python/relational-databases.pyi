968c014a84d2a0cad7917d189b5fad96c0687aab
petsereypanha <sereypanhapet@gmail.com>
2025-11-08 20:47:53 +0700

    introduction  importing data in python

--- importing-data-in-python/relational-databases.pyi
+++ importing-data-in-python/relational-databases.pyi
@@ -110,3 +110,23 @@ df = pd.read_sql_query(
 # Print head of DataFrame
 print(df.head())
 
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute(
+        "SELECT Album.Title, Artist.Name FROM Album INNER JOIN Artist ON Album.ArtistID = Artist.ArtistID"
+    )
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query(
+    "SELECT * FROM PlaylistTrack INNER JOIN Track ON PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000;",
+    engine
+)
+
+# Print head of DataFrame
+print(df.head())
