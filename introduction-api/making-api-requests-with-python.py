5cfa29ccf5bce2a7ef05e69d9c26a057b97959ee
petsereypanha <sereypanhapet@gmail.com>
2025-11-10 22:47:41 +0700

    the basic anatomy of an api request

--- introduction-api/making-api-requests-with-python.py
+++ introduction-api/making-api-requests-with-python.py
@@ -18,3 +18,50 @@ response = requests.get('http://localhost:3000/lyrics/')
 
 # Print out the text attribute of the response object
 print(response.text)
+
+# Construct the URL string and pass it to the requests.get() function
+response = requests.get('http://localhost:3000/lyrics/random')
+
+print(response.text)
+
+# Create a dictionary variable with query params
+query_params = { 'artist': 'Deep Purple'}
+
+# Pass the dictionary to the get() function
+response = requests.get('http://localhost:3000/lyrics/random', params=query_params)
+
+print(response.text)
+
+# Add the `include_track` parameter
+query_params = { 'artist': 'Deep Purple', 'include_track': True}
+
+response = requests.get('http://localhost:3000/lyrics/random', params=query_params)
+
+# Print the response URL
+print(response.url)
+
+# Print the lyric
+print(response.text)
+
+# Get a list of all playlists from the API
+response = requests.get('http://localhost:3000/playlists')
+print(response.text)
+
+# Create a dictionary with the playlist info
+playlist_data = {"Name": 'Rock Ballads'}
+
+# Perform a POST request to the playlists API with your dictionary as data parameter
+response = requests.post('http://localhost:3000/playlists', data=playlist_data)
+print(response.text)
+
+# Perform a GET request to get info on playlist with PlaylistId 2
+response = requests.get('http://localhost:3000/playlists/2')
+
+print(response.text)
+
+# Perform a DELETE request to the playlist API using the path to playlist with PlaylistId 2
+requests.delete('http://localhost:3000/playlists/2')
+
+# Get the list of all existing playlists again
+response = requests.get('http://localhost:3000/playlists')
+print(response.text)
