e83110655fd8812e6ab0d4d5823c022233221500
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 10:38:35 +0700

    structure data

--- introduction-api/api-request-concepts.py
+++ introduction-api/api-request-concepts.py
@@ -38,3 +38,30 @@ elif(response.status_code == 401):
 else:
     print('Another error occurred')
 
+# Store the API token in a variable
+API_TOKEN = '8apDFHaNJMxy8Kt818aa6b4a0ed0514b5d3'
+
+headers = {
+    'Authorization': 'Bearer ' + API_TOKEN,
+    # Add a header to request JSON formatted data
+    'Accept': 'application/json'
+}
+response = requests.get('http://localhost:3000/albums/1/', headers=headers)
+
+# Get the JSON data as a Python object from the response object
+album = response.json()
+
+# Print the album title
+print(album['Title'])
+
+
+playlists = [{"Name":"Rock ballads"}, {"Name":"My favorite songs"}, {"Name":"Road Trip"}]
+
+# POST the playlists array to the API using the json argument
+requests.post('http://localhost:3000/playlists/', json=playlists)
+
+# Get the list of all created playlists
+response = requests.get('http://localhost:3000/playlists')
+
+# Print the response text to inspect the JSON text
+print(response.text)
