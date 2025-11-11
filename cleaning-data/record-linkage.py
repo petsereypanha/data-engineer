a3518505c8c4fd06ad4aa5ce77f6b0d864883dfd
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 16:28:29 +0700

    linking data frames

--- cleaning-data/record-linkage.py
+++ cleaning-data/record-linkage.py
@@ -50,3 +50,30 @@ for cuisine in ['asian', 'american', 'italian']:
 
 # Inspect the final result
 print(restaurants['cuisine_type'].unique())
+
+import recordlinkage
+
+restaurants_new = pd.read_csv('data/restaurants_new.csv')
+
+# Create an indexer and object and find possible pairs
+indexer = recordlinkage.Index()
+
+# Block pairing on cuisine_type
+indexer.block('cuisine_type')
+
+# Generate pairs
+pairs = indexer.index(restaurants, restaurants_new)
+
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Find exact matches on city, cuisine_type
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label='cuisine_type')
+
+# Find similar matches of rest_name
+comp_cl.string('rest_name', 'rest_name', label='name', threshold=0.8)
+
+# Get potential matches and print
+potential_matches = comp_cl.compute(pairs, restaurants, restaurants_new)
+print(potential_matches)
