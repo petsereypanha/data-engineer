bd438af8e6cd573472241de18a68e00f885743f6
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 12:18:47 +0700

    date range contraiants

--- cleaning-data/common-data-problems.py
+++ cleaning-data/common-data-problems.py
@@ -37,3 +37,30 @@ assert ride_sharing['duration_time'].dtype == 'int'
 # Print formed columns and calculate average ride duration
 print(ride_sharing[['duration','duration_trim','duration_time']])
 print(ride_sharing['duration_time'].mean())
+
+# Convert tire_sizes to integer
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('int')
+
+# Set all values above 27 to 27
+ride_sharing.loc[ride_sharing['tire_sizes'] > 27, 'tire_sizes'] = 27
+
+# Reconvert tire_sizes back to categorical
+ride_sharing['tire_sizes'] = ride_sharing['tire_sizes'].astype('category')
+
+# Print tire size description
+print(ride_sharing['tire_sizes'].describe())
+
+# import datetime module
+import datetime as dt
+
+# Convert ride_date to date
+ride_sharing['ride_dt'] = pd.to_datetime(ride_sharing['ride_date']).dt.date
+
+# Save today's date
+today = dt.date.today()
+
+# Set all in the future to today's date
+ride_sharing.loc[ride_sharing['ride_dt'] > today, 'ride_dt'] = today
+
+# Print maximum of ride_dt column
+print(ride_sharing['ride_dt'].max())
