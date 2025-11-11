d3a7a4101785c3cb31fa00658656eed72f9e0458
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 14:56:53 +0700

    uniformity

---
+++ cleaning-data/advanced-data-problems.py
@@ -0,0 +1,29 @@
+# load necessary libraries
+import pandas as pd
+# load banking data
+banking = pd.read_csv('data/banking1.csv')
+# Find values of acct_cur that are equal to 'euro'
+acct_eu = banking['acct_cur'] == 'euro'
+
+# Convert acct_amount where it is in euro to dollars
+banking.loc[acct_eu, 'acct_amount'] = banking.loc[acct_eu, 'acct_amount'] * 1.1
+
+# Unify acct_cur column by changing 'euro' values to 'dollar'
+banking.loc[acct_eu, 'acct_cur'] = 'dollar'
+
+# Assert that only dollar currency remains
+assert banking['acct_cur'].unique() == 'dollar'
+
+# Print the header of account_opend
+print(banking['account_opened'].head())
+
+# Convert account_opened to datetime
+banking['account_opened'] = pd.to_datetime(banking['account_opened'],
+                                           # Return missing value for error
+                                           errors = 'coerce')
+
+# Get year of account opened
+banking['acct_year'] = banking['account_opened'].dt.strftime('%Y')
+
+# Print acct_year
+print(banking['acct_year'])
