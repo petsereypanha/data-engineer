bddbd4c3528ccab45abed3bb71feba289a912977
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 14:05:07 +0700

    categories variables

--- cleaning-data/text-and-categorical-data-problems.py
+++ cleaning-data/text-and-categorical-data-problems.py
@@ -33,3 +33,35 @@ print(airlines[cat_clean_rows])
 
 # Print rows with consistent categories only
 print(airlines[~cat_clean_rows])
+
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower()
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Import numpy
+import numpy as np
+
+# Create ranges for categories
+label_ranges = [0, 60, 180, np.inf]
+label_names = ['short', 'medium', 'long']
+
+# Create wait_type column
+airlines['wait_type'] = pd.cut(airlines['wait_min'], bins=label_ranges, labels=label_names)
+
+# Create mappings and replace
+mappings = {'Monday':'weekday', 'Tuesday':'weekday', 'Wednesday':'weekday',
+            'Thursday':'weekday', 'Friday':'weekday',
+            'Saturday':'weekend', 'Sunday':'weekend'}
+
+airlines['day_week'] = airlines['day'].replace(mappings)
