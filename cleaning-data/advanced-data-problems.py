9a1ccfbdd011a3aeb06f7d0e79bab538b0934ac9
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 15:41:33 +0700

    completeness

--- cleaning-data/advanced-data-problems.py
+++ cleaning-data/advanced-data-problems.py
@@ -66,3 +66,34 @@ inconsistent_ages = banking[~age_equ]
 
 # Print number of inconsistent ages
 print("Number of inconsistent ages: ", inconsistent_ages.shape[0])
+
+import missingno as msno
+import matplotlib.pyplot as plt
+
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values(by='Age')
+msno.matrix(banking_sorted)
+plt.show()
+
+# Drop missing values of cust_id
+banking_fullid = banking.dropna(subset = ['cust_id'])
+
+# Compute estimated acct_amount
+acct_imp = banking_fullid['inv_amount'] * 5
+
+# Impute missing acct_amount with corresponding acct_imp
+banking_imputed = banking_fullid.fillna({'acct_amount': acct_imp})
+
+# Print number of missing values
+print(banking_imputed.isna().sum())
