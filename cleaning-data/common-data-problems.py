74e21820f1d8c37a23962222f4c43c12086059e4
petsereypanha <sereypanhapet@gmail.com>
2025-11-11 11:58:01 +0700

    data type contraints

---
+++ cleaning-data/common-data-problems.py
@@ -0,0 +1,39 @@
+# Import necessary libraries
+import pandas as pd
+
+# Load the ride_sharing_data.csv file into a DataFrame
+ride_sharing = pd.read_csv('data/ride_sharing.csv')
+
+# Print the information of ride_sharing
+print(ride_sharing.info())
+
+# Print summary statistics of user_type column
+print(ride_sharing['user_type'].describe())
+
+# Print the information of ride_sharing
+print(ride_sharing.info())
+
+# Print summary statistics of user_type column
+print(ride_sharing['user_type'].describe())
+
+# Convert user_type from integer to category
+ride_sharing['user_type_cat'] = ride_sharing['user_type'].astype('category')
+
+# Write an assert statement confirming the change
+assert ride_sharing['user_type_cat'].dtype == 'category'
+
+# Print new summary statistics
+print(ride_sharing['user_type_cat'].describe())
+
+# Strip 'minutes' from duration
+ride_sharing['duration_trim'] = ride_sharing['duration'].str.strip('minutes')
+
+# Convert duration_trim to integer
+ride_sharing['duration_time'] = ride_sharing['duration_trim'].astype(int)
+
+# Write an assert statement making sure of conversion
+assert ride_sharing['duration_time'].dtype == 'int'
+
+# Print formed columns and calculate average ride duration
+print(ride_sharing[['duration','duration_trim','duration_time']])
+print(ride_sharing['duration_time'].mean())
