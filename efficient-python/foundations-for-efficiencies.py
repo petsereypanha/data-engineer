fd53f7ae8e06b1cee3ec4c62f23772d82218cc03
petsereypanha <sereypanhapet@gmail.com>
2025-11-12 10:45:26 +0700

    biulding with build in

---
+++ efficient-python/foundations-for-efficiencies.py
@@ -0,0 +1,61 @@
+# import necessary modules
+
+
+# Print the list created using the Non-Pythonic approach
+names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
+i = 0
+new_list= []
+while i < len(names):
+    if len(names[i]) >= 6:
+        new_list.append(names[i])
+    i += 1
+print(new_list)
+
+better_list = []
+for name in names:
+    if len(name) >= 6:
+        better_list.append(name)
+print(better_list)
+
+# Print the list created by using list comprehension
+best_list = [name for name in names if len(name) >= 6]
+print(best_list)
+
+# Create a range object that goes from 0 to 5
+nums = range(5)
+print(type(nums))
+
+# Convert nums to a list
+nums_list = list(nums)
+print(nums_list)
+
+# Create a new list of odd numbers from 1 to 11 by unpacking a range object
+nums_list2 = [*range(1, 12, 2)]
+print(nums_list2)
+
+# Rewrite the for loop to use enumerate
+indexed_names = []
+for i,name in enumerate(names):
+    index_name = (i,name)
+    indexed_names.append(index_name)
+print(indexed_names)
+
+# Rewrite the above for loop using list comprehension
+indexed_names_comp = [(i,name) for i,name in enumerate(names)]
+print(indexed_names_comp)
+
+# Unpack an enumerate object with a starting index of one
+indexed_names_unpack = [*enumerate(names, 1)]
+print(indexed_names_unpack)
+
+# Use map to apply str.upper to each element in names
+names_map  = map(str.upper, names)
+
+# Print the type of the names_map
+print(type(names_map))
+
+# Unpack names_map into a list
+names_uppercase = [*names_map]
+
+# Print the list created above
+print(names_uppercase)
