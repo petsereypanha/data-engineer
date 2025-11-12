90d753bb425b239f91b1cebf9d33466f61a9852a
petsereypanha <sereypanhapet@gmail.com>
2025-11-12 11:05:29 +0700

    numpy array

--- efficient-python/foundations-for-efficiencies.py
+++ efficient-python/foundations-for-efficiencies.py
@@ -1,5 +1,5 @@
 # import necessary modules
-
+import numpy as np
 
 # Print the list created using the Non-Pythonic approach
 names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
@@ -59,3 +59,53 @@ names_uppercase = [*names_map]
 
 # Print the list created above
 print(names_uppercase)
+
+# Print second row of nums
+print(nums[1, :])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums with each element + 1
+nums[:, 2] = nums[:, 2] + 1
+print(nums)
+
+# Create a list of arrival times
+arrival_times = [*range(10,60,10)]
+
+print(arrival_times)
+
+# Create a list of arrival times
+arrival_times = [*range(10,60,10)]
+
+# Convert arrival_times to an array and update the times
+arrival_times_np = np.array(arrival_times)
+new_times = arrival_times_np - 3
+
+print(new_times)
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i],time) for i,time in enumerate(new_times)]
+
+print(guest_arrivals)
+
+# Create a list of arrival times
+arrival_times = [*range(10,60,10)]
+
+# Convert arrival_times to an array and update the times
+arrival_times_np = np.array(arrival_times)
+new_times = arrival_times_np - 3
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i],time) for i,time in enumerate(new_times)]
+
+# Map the welcome_guest function to each (guest,time) pair
+welcome_map = map(welcome_guest, guest_arrivals)
+
+guest_welcomes = [*welcome_map]
+print(*guest_welcomes, sep='\n')
+
