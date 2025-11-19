c1852957ce5be2b9f11fcc9ba21b60d1d464a066
petsereypanha <sereypanhapet@gmail.com>
2025-11-19 19:35:49 +0700

    Software Engineering & Data Science

---
+++ principles-python/introduction.py
@@ -0,0 +1,60 @@
+# import the numpy package
+import numpy as np
+
+# create an array class object
+arr = np.array([8, 6, 7, 5, 3, 0, 9])
+
+# use the sort method
+arr.sort()
+
+# print the sorted array
+print(arr)
+
+# load the Counter function into our environment
+from collections import Counter
+
+# View the documentation for Counter.most_common
+help(Counter.most_common)
+
+# use Counter to find the top 5 most common words
+top_5_words = Counter(
+    ["apple", "banana", "apple", "orange", "banana", "apple", "kiwi", "banana", "banana"]
+).most_common(5)
+
+# display the top 5 most common words
+print(top_5_words)
+
+# Import needed package
+import pycodestyle
+
+# Create a StyleGuide instance
+style_checker = pycodestyle.StyleGuide()
+
+# Run PEP 8 check on multiple files
+result = style_checker.check_files(['nay_pep8.py', 'yay_pep8.py'])
+
+# Print result of PEP 8 style check
+print(result.messages)
+
+# Assign data to x
+x = [8, 3, 4]
+
+# Print the data
+print(x)
+
+def print_phrase(phrase, polite=True, shout=False):
+    if polite:  # It's generally polite to say please
+        phrase = 'Please ' + phrase
+
+    if shout:  # All caps looks like a written shout
+        phrase = phrase.upper() + '!!'
+
+    print(phrase)
+
+
+# Politely ask for help
+print_phrase('help me', polite=True)
+# Shout about a discovery
+print_phrase('eureka', shout=True)
+
+
