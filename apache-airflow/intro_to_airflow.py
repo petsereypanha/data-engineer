da17de0e264bfcee762cc2f2c82fef366ab91653
petsereypanha <sereypanhapet@gmail.com>
2025-11-20 14:28:10 +0700

    implementing airflow dags

--- apache-airflow/intro_to_airflow.py
+++ apache-airflow/intro_to_airflow.py
@@ -102,3 +102,15 @@ email_manager_task = EmailOperator(
 # Set the order of tasks
 pull_file_task >> parse_file_task >> email_manager_task
 
+default_args = {
+  'owner': 'Engineering',
+  'start_date': datetime(2023, 11, 1),
+  'email': ['airflowresults@datacamp.com'],
+  'email_on_failure': False,
+  'email_on_retry': False,
+  'retries': 3,
+  'retry_delay': timedelta(minutes=20)
+}
+
+dag = DAG('update_dataflows', default_args=default_args, schedule_interval='30 12 * * 3')
+
