4b0364fca59ab2ccfbe1f8c1383931c458177167
petsereypanha <sereypanhapet@gmail.com>
2025-11-29 19:18:39 +0700

    relational plots and subplots

---
+++ seaborn_data_visualization/visualizing_two_quantitative_variables.py
@@ -0,0 +1,29 @@
+# Import necessary libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# Load student data
+student_data = pd.read_csv('../data/student.csv')
+
+# Change to use relplot() instead of scatterplot()
+sns.scatterplot(x="absences", y="G3",
+                data=student_data,
+                kind="scatter",
+                col="study_time",)
+
+# Show plot
+plt.show()
+
+
+# Create a scatter plot of G1 vs. G3
+sns.relplot(x="G1", y="G3",
+            data=student_data,
+            kind="scatter",
+            col="schoolsup",
+            row="famsup",
+            col_order=["yes", "no"],
+            row_order=["yes", "no"])
+
+# Show plot
+plt.show()
