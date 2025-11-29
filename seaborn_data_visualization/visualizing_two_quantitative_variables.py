a32d28d479ef1b28d0e25a4bd60a4a8ce4427063
petsereypanha <sereypanhapet@gmail.com>
2025-11-29 20:34:09 +0700

    visualizing two quantitative variables

--- seaborn_data_visualization/visualizing_two_quantitative_variables.py
+++ seaborn_data_visualization/visualizing_two_quantitative_variables.py
@@ -63,4 +63,73 @@ sns.relplot(
 # Show plot
 plt.show()
 
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Create line plot
+sns.relplot(
+    x="model_year",
+    y="mpg",
+    data=mpg,
+    kind="line",
+    hue="origin",
+    style="origin",
+    markers=True,
+    dashes=False
+)
+
+
+# Show plot
+plt.show()
+
+
+# Make the shaded area show the standard deviation
+sns.relplot(x="model_year", y="mpg",
+            data=mpg, kind="line",ci="sd")
+
+# Show plot
+plt.show()
+
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create line plot of model year vs. horsepower
+sns.relplot(
+    x="model_year",
+    y="horsepower",
+    data=mpg,
+    kind="line",
+    ci=None
+)
+
+# Show plot
+plt.show()
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Change to create subgroups for country of origin
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None,  hue="origin",
+    style="origin")
+
+# Show plot
+plt.show()
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Add markers and make each line have the same style
+sns.relplot(x="model_year", y="horsepower",
+            data=mpg, kind="line",
+            ci=None, style="origin",
+            hue="origin", markers=True, dashes=False)
+
+# Show plot
+plt.show()
