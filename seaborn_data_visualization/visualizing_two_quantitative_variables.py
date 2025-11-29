63ba51b21958eee4aad356176d2329c8a5f1940a
petsereypanha <sereypanhapet@gmail.com>
2025-11-29 20:25:30 +0700

    scatter plot

--- seaborn_data_visualization/visualizing_two_quantitative_variables.py
+++ seaborn_data_visualization/visualizing_two_quantitative_variables.py
@@ -27,3 +27,40 @@ sns.relplot(x="G1", y="G3",
 
 # Show plot
 plt.show()
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mpg = pd.read_csv('../data/mpg.csv')
+
+# Create scatter plot of horsepower vs. mpg
+sns.relplot(x="horsepower",
+    y="mpg",
+    data=mpg,
+    kind="scatter",
+    size="cylinders",
+    hue="cylinders")
+
+
+# Show plot
+plt.show()
+
+# Import Matplotlib and Seaborn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create a scatter plot of acceleration vs. mpg
+sns.relplot(
+    x="acceleration",
+    y="mpg",
+    data=mpg,
+    kind="scatter",
+    hue="origin",
+    style="origin"
+)
+
+# Show plot
+plt.show()
+
+
