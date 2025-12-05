df40c35592af98458577feb10006c9af5580d360
petsereypanha <sereypanhapet@gmail.com>
2025-12-06 00:56:56 +0700

    non parametric test

--- hypothesis_testing/Non-Parametric_Tests.py
+++ hypothesis_testing/Non-Parametric_Tests.py
@@ -75,3 +75,27 @@ wilcoxon_test_results = pingouin.wilcoxon(
 
 # Print Wilcoxon test results
 print(wilcoxon_test_results)
+
+# Select the weight_kilograms and late columns
+weight_vs_late = late_shipments[['weight_kilograms', 'late']]
+
+# Convert weight_vs_late into wide format
+weight_vs_late_wide = weight_vs_late.pivot(columns='late', values='weight_kilograms')
+
+
+# Run a two-sided Wilcoxon-Mann-Whitney test on weight_kilograms vs. late
+wmw_test = pingouin.mwu(x=weight_vs_late_wide['Yes'], y=weight_vs_late_wide['No'], alternative='two-sided')
+
+
+# Print the test results
+print(wmw_test)
+
+# Run a Kruskal-Wallis test on weight_kilograms vs. shipment_mode
+kw_test = pingouin.kruskal(
+    data=late_shipments,
+    dv='weight_kilograms',
+    between='shipment_mode'
+)
+
+# Print the results
+print(kw_test)
