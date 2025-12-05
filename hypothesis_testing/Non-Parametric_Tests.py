81ff3fa6e61d6737c8425380b787290e50a32c35
petsereypanha <sereypanhapet@gmail.com>
2025-12-06 00:47:46 +0700

    Non-parametric

--- hypothesis_testing/Non-Parametric_Tests.py
+++ hypothesis_testing/Non-Parametric_Tests.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from scipy.stats import t
 import matplotlib.pyplot as plt
-import pingouin as pg
+import pingouin as pingouin
 import seaborn as sns
 
 def load_dataset(file_path):
@@ -55,3 +55,23 @@ print(counts)
 
 # Inspect whether the counts are big enough
 print((counts >= 30).all())
+
+# Conduct a paired t-test on dem_percent_12 and dem_percent_16
+paired_test_results = pingouin.ttest(
+    sample_dem_data['dem_percent_12'],
+    sample_dem_data['dem_percent_16'],
+    paired=True,
+    alternative='two-sided'
+)
+# Print paired t-test results
+print(paired_test_results)
+
+# Conduct a Wilcoxon test on dem_percent_12 and dem_percent_16
+wilcoxon_test_results = pingouin.wilcoxon(
+    sample_dem_data['dem_percent_12'],
+    sample_dem_data['dem_percent_16'],
+    alternative='two-sided'
+)
+
+# Print Wilcoxon test results
+print(wilcoxon_test_results)
